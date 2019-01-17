743eb454f9754b34341d15dc45db202d30f041f8
pawanbangalala <pawan.bangalala@gmail.com>
2019-01-17 18:59:55 +0530

    test

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
-		backgroundColor: '#fff',
+		backgroundColor: '#ffa',
 	},
 	welcome: {
 		fontSize: 20,
